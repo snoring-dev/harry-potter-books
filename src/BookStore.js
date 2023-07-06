d888748210e75ea4542b0d12ba7c2c5eac5f1277
snoring-dev <jemmoudi.med@proton.me>
2023-07-06 07:41:31 +0100

    feat(kata): combaining two books and one book cases

--- src/BookStore.js
+++ src/BookStore.js
@@ -3,20 +3,54 @@ import { SINGLE_BOOK_PRICE } from "./Constants";
 export default class BookStore {
   constructor() {
     this.books = new Map();
-    this.numberOfBooks = 0;
   }
 
   getTotalPrice() {
-    const numberOfDistinctBooks = this.getNumberOfDistinctBooks();
-    if (numberOfDistinctBooks === 2) {
-      return numberOfDistinctBooks * SINGLE_BOOK_PRICE * 0.95;
+    const remainingBooks = new Map(this.books);
+    let numberOfBooks = this.getNumberOfBooks(remainingBooks);
+    let numberOfDistinctBooks = this.getNumberOfDistinctBooks(remainingBooks);
+    let totalPrice = 0;
+
+    while (numberOfBooks > 0) {
+      if (numberOfDistinctBooks === 2) {
+        totalPrice += 2 * SINGLE_BOOK_PRICE * 0.95;
+      } else {
+        totalPrice += SINGLE_BOOK_PRICE;
+      }
+
+      this.removeOneBookOnEachVolume(remainingBooks);
+      numberOfBooks = this.getNumberOfBooks(remainingBooks);
+      numberOfDistinctBooks = this.getNumberOfDistinctBooks(remainingBooks);
     }
 
-    return this.numberOfBooks * SINGLE_BOOK_PRICE;
+    return totalPrice;
   }
 
-  getNumberOfDistinctBooks() {
-    return this.books.size;
+  removeOneBookOnEachVolume(booksOrder) {
+    const distinctBooksKeys = [...booksOrder.keys()];
+    for (let i = 0; i < distinctBooksKeys.length; i++) {
+      const book = Number(distinctBooksKeys[i]);
+      const amount = Number(booksOrder.get(book));
+      if (amount <= 1) {
+        booksOrder.delete(Number(book));
+      } else {
+        booksOrder.set(Number(book), amount - 1);
+      }
+    }
+  }
+
+  getNumberOfDistinctBooks(remainingBooks) {
+    return remainingBooks.size;
+  }
+
+  getNumberOfBooks(remainingBooks) {
+    let booksCount = 0;
+
+    if (remainingBooks && remainingBooks.size > 0) {
+        remainingBooks.forEach((value) => booksCount += value);
+    }
+    
+    return booksCount;
   }
 
   addBook(volume) {
@@ -25,6 +59,5 @@ export default class BookStore {
       amount = this.books.get(volume);
     }
     this.books.set(volume, amount + 1);
-    this.numberOfBooks += 1;
   }
 }
